54f0429c382a9761ba7be5c97040ebe71860be7c
Alauddin <alauddin3604@gmail.com>
2020-04-06 08:41:10 +0800

    Renamed

--- admin/admin.php
+++ admin/admin.php
@@ -4,21 +4,21 @@ session_start();
 date_default_timezone_set('Asia/Kuala_Lumpur');
 $msg = "";
 
-if (isset($_SESSION['adm_id']))
-	$adm_id = $_SESSION['adm_id'];
+if (isset($_SESSION['session_id']))
+	$session_id = $_SESSION['session_id'];
 else
 	header('location: ../index.php');
 
-$q = "SELECT Adm_ID, Adm_Name FROM admin WHERE Adm_ID = '$adm_id'";
+$q = "SELECT Adm_ID, Adm_Name FROM admin WHERE Adm_ID = '$session_id'";
 if(!$result = $conn->query($q)) {
 	echo $conn->error;
 }
 else {
 	$row = $result->fetch_assoc();
-	$adm_name = $row['Adm_Name'];
+	$session_name = $row['Adm_Name'];
 }
 
-if(isset($_POST['add']))
+if(isset($_POST['add'])) // Add new data
 {
 	if (empty($_POST['adm_id']) || empty($_POST['adm_name'])) {
 		$msg = '<div class="w3-panel w3-pale-red w3-display-container">
@@ -54,6 +54,40 @@ if(isset($_POST['add']))
 	}
 }
 
+if(isset($_POST['delete'])) {// Delete admin
+	$adm_id = $_POST['adm_id'];
+
+	$sql = "SELECT a.Adm_ID, b.Adm_ID, c.Adm_ID
+			FROM adm_lect a, adm_stud b, adm_sub c
+			WHERE  a.Adm_ID = '$adm_id' OR b.Adm_ID = '$adm_id' OR c.Adm_ID = '$adm_id'";
+	
+	
+	if (!$result = $conn->query($sql)) {
+		echo $conn->error;
+	}
+	else {
+		if ($result->num_rows > 0) {
+			$msg = '<div class="w3-panel w3-pale-red w3-display-container">
+			<span onclick="this.parentElement.style.display=\'none\'"
+			class="w3-button w3-large w3-display-topright">&times;</span>
+			<h3>Unsuccessful!</h3>
+			<p>The admin data is currently used in other tables.</p>
+			</div>';
+		}
+		else {
+			$sql = "DELETE FROM admin WHERE Adm_ID = '$adm_id'";
+
+			if ($conn->query($sql)) {
+				$msg = '<div class="w3-panel w3-pale-green w3-display-container">
+				<span onclick="this.parentElement.style.display=\'none\'"
+				class="w3-button w3-large w3-display-topright">&times;</span>
+				<h3>Successful!</h3>
+				<p>Data is deleted successfully.</p>
+				</div>';
+			}
+		}
+	}	
+}
 ?>
 
 <!DOCTYPE html>
@@ -82,7 +116,7 @@ if(isset($_POST['add']))
 			<a href="workload-reg.php" class="w3-bar-item w3-button">Workload</a>
 			<a href="../logout.php" class="w3-bar-item w3-button w3-right">Log Out</a>
 		</div>
-		<p>Current session: <?php echo $adm_id.", ".$adm_name ?></p>
+		<p>Current session: <?php echo $session_id.", ".$session_name ?></p>
 		<br>
 		<br>
 		<table class="w3-table w3-bordered">
@@ -103,8 +137,11 @@ if(isset($_POST['add']))
 						<td><?php echo $row['Adm_ID']; ?></td>
 						<td><?php echo $row['Adm_Name']; ?></td>
 						<td><a href="student-update.php?id=<?php echo $row["Adm_ID"]; ?>"><button>Update</button></a></td>
-						<?php if ($row['Adm_ID'] != $adm_id) { ?>
-							<td><a href="delete.php?adm_id=<?php echo $row["Adm_ID"]; ?>"><button>Delete</button></a></td>				
+						<?php if ($row['Adm_ID'] != $session_id) { ?>
+							<form action="" method="POST">
+								<input type="text" name="adm_id" value="<?php echo $row["Adm_ID"]; ?>" hidden />
+								<td><button type="submit" name="delete">Delete</button></td>
+							</form>				
 							<?php } else { echo "<td></td>"; }?>
 						</tr>
 						<?php
@@ -122,10 +159,5 @@ if(isset($_POST['add']))
 		</table>
 		<p><?php echo $msg; ?></p>
 	</div>
-	<script>    
-	if(typeof window.history.pushState == 'function') {
-		window.history.pushState({}, "Hide", "http://localhost:8012/project-db/admin/admin-reg.php");
-	}
-	</script>
 </body>
 </html>
