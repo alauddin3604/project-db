d91fc661b2c38ce924feb786b1de35d29062b3ec
Alauddin <alauddin3604@gmail.com>
2020-04-15 01:01:27 +0800

    Debug SQL syntax

--- pass-update.php
+++ pass-update.php
@@ -1,44 +1,50 @@
 <?php
 require 'connection.php';
 session_start();
-
-if (isset($_SESSION['session_id']))
-	$welcome_id = $_SESSION['session_id'];
-else if (isset($_SESSION['stud_id']))
-	$welcome_id = $_SESSION['stud_id'];
-else if (isset($_SESSION['lect_id']))
-	$welcome_id = $_SESSION['lect_id'];
+$msg = '';
+if (isset($_SESSION['admin_id']))
+	$welcome_id = $_SESSION['admin_id'];
+else if (isset($_SESSION['student_id']))
+	$welcome_id = $_SESSION['student_id'];
+else if (isset($_SESSION['lecturer_id']))
+	$welcome_id = $_SESSION['lecturer_id'];
 	
 
-if(isset($_POST['update']))
+if (isset($_POST['update']))
 {
-	if (isset($_SESSION['stud_id']))
+	if (trim($_POST['new_pass']) != trim($_POST['confirm_pass'])) // Check if passwords are matching 
 	{
-		$stud_id = $_SESSION['stud_id'];
-
-		if(trim($_POST['new_pass']) == trim($_POST['confirm_pass']))
+		$msg = '<p style="color: red;">*Password doesn\'t match!</p>';
+	}
+	else
+	{
+		if (isset($_SESSION['student_id'])) // If current session is student
 		{
-			$new_pass = password_hash($_POST['new_pass'], PASSWORD_DEFAULT);
-			$sql = "UPDATE student SET Stud_Pass = '$new_pass' WHERE Stud_ID = '$stud_id'";
-			if($conn->query($sql))
-			{
+			$student_id = $_SESSION['student_id'];
+
+			$student_password = password_hash($_POST['new_pass'], PASSWORD_DEFAULT);
+			$sql = 'UPDATE students SET student_password = ? WHERE student_id = ?';
+			$stmt = $conn->prepare($sql);
+			$stmt->bind_param('ss', $student_password, $student_id);
+			
+			if($stmt->execute())
+	 		{
 				header('location: student/home.php');
-			}
+	 		}
 			else
-			{
-				echo $conn->error;
+	 		{
+	 			echo $conn->error;
 			}
 		}
-	}
-	else if (isset($_SESSION['lect_id'])) {
-		
-		$lect_id = $_SESSION['lect_id'];
-
-		if(trim($_POST['new_pass']) == trim($_POST['confirm_pass']))
+		else if (isset($_SESSION['lecturer_id'])) // If current session is lecturer
 		{
-			$new_pass = password_hash($_POST['new_pass'], PASSWORD_DEFAULT);
-			$sql = "UPDATE lecturer SET Lect_Pass = '$new_pass' WHERE Lect_ID = '$lect_id'";
-			if($conn->query($sql))
+			$lecturer_id = $_SESSION['lecturer_id'];
+
+			$lecturer_password = password_hash($_POST['new_pass'], PASSWORD_DEFAULT);
+			$sql = 'UPDATE lecturers SET lecturer_password = ? WHERE lecturer_id = ?';
+			$stmt = $conn->prepare($sql);
+			$stmt->bind_param('si', $lecturer_password, $lecturer_id);
+			if($stmt->execute())
 			{
 				header('location: lecturer/home.php');
 			}
@@ -46,17 +52,17 @@ if(isset($_POST['update']))
 			{
 				echo $conn->error;
 			}
-		}
-	}
-	else if (isset($_SESSION['session_id'])) {
-		
-		$adm_id = $_SESSION['session_id'];
 
-		if(trim($_POST['new_pass']) == trim($_POST['confirm_pass']))
+		} 
+		else if (isset($_SESSION['admin_id'])) // If current session is admin
 		{
-			$new_pass = password_hash($_POST['new_pass'], PASSWORD_DEFAULT);
-			$sql = "UPDATE admin SET Adm_Pass = '$new_pass' WHERE Adm_ID = '$adm_id'";
-			if($conn->query($sql))
+			$admin_id = $_SESSION['admin_id'];
+
+			$admin_password = password_hash($_POST['new_pass'], PASSWORD_DEFAULT);
+			$sql = 'UPDATE admins SET admin_password = ? WHERE admin_id = ?';
+			$stmt = $conn->prepare($sql);
+			$stmt->bind_param('si', $admin_password, $admin_id);
+			if($stmt->execute())
 			{
 				header('location:admin/home.php');
 			}
@@ -101,6 +107,7 @@ if(isset($_POST['update']))
 			<label for="submit"></label>
 			<input type="submit" name="update" id="submit" value="Update">
 		</form>
+		<?php echo $msg; ?>
 	</div>	
 </body>
 </html>
